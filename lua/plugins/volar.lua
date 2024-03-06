1965a64366c77c1c7f574e80fe09a252957617c3
Hiemanshu Sharma <hiemanshu@gmail.com>
2024-03-06 16:27:19 +0530

    Use new TS Vue setup

--- lua/plugins/volar.lua
+++ lua/plugins/volar.lua
@@ -3,22 +3,26 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        volar = {
-          -- Takeover mode https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#volar
-          filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+        tsserver = {
+          init_options = {
+            hostInfo = "neovim",
+            plugins = {
+              {
+                name = "@vue/typescript-plugin",
+                location = "anything", -- makes it look in node_modules
+                languages = {
+                  "typescript",
+                  "vue",
+                },
+              },
+            },
+          },
+          filetypes = {
+            "javascript",
+            "typescript",
+            "vue",
+          },
         },
-      },
-      setup = {
-        volar = function()
-          require("lazyvim.util").lsp.on_attach(function(client)
-            -- Don't use volar for formatting
-            if client.name == "volar" then
-              client.server_capabilities.documentFormatting = false
-              -- client.server_capabilities.documentFeatures.documentFormatting = false
-              client.server_capabilities.documentFormattingProvider = false
-            end
-          end)
-        end,
       },
     },
   },
