1eb5a41664e5dd11ce4d5c2521331c0bee0b6a50
Hiemanshu Sharma <hiemanshu@gmail.com>
2024-05-27 14:42:54 +0530

    Update to latest lazyvim, disable window blur

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,4 +6,5 @@ if vim.g.neovide then
   vim.opt.guifont = { "CommitMono Nerd Font", ":h17" }
   vim.g.neovide_confirm_quit = true
   vim.opt.linespace = 1
+  vim.g.neovide_window_blurred = false
 end
