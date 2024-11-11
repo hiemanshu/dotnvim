9110fb2bcee5007f52e6694f9f4212b5e3ec2d4b
Hiemanshu Sharma <hiemanshu@gmail.com>
2024-11-11 12:38:10 +0530

    Update current theme

--- lua/plugins/gui.lua
+++ lua/plugins/gui.lua
@@ -1,10 +1,12 @@
 return {
   { "rose-pine/neovim", name = "rose-pine" },
-  { "EdenEast/nightfox.nvim" },
+  { "EdenEast/nightfox.nvim", lazy = false, priority = 1000 },
+  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+  { "folke/tokyonight.nvim", lazy = false, priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "carbonfox",
+      colorscheme = "tokyonight-night",
     },
   },
 }
